df4ec0f34b81c2cc5be1696552a720cf7a61140b
Dale Hewgill <dhewgill@gmail.com>
2015-11-02 14:55:51 -0800

    Added explicit 'void' arg to _system_pre_init function declaration.

--- main.c
+++ main.c
@@ -206,7 +206,7 @@ DateTime_t						gDt;
 // It's a good place to stop the watchdog and set the system freq, etc...
 // Just don't do anything that requires global variables yet!
 // The 'return 1' is necessary to init globals on function exit.
-int _system_pre_init()
+int _system_pre_init(void)
 {
     WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
 
